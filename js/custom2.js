a0ed66363d6c4c83747d027c16d48dcd56a28211
clairefrebault <clairefblt@gmail.com>
2016-02-22 20:12:27 +0100

    Added files via upload

---
+++ js/custom2.js
@@ -0,0 +1,11 @@
+$( document ).ready(function(){ 
+var winHeight = $(window).height(); // hauteur de la fenêtre
+console.log('Hauteur de la fenêtre : '+ winHeight);
+$('#page').css('min-height', winHeight); // <header> prend la hauteur de la fenêtre
+});
+
+$(document).ready(function(){
+    $('.bxslider').bxSlider({
+      adaptiveHeight: true
+    });
+});
